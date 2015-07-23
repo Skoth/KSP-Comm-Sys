5a1a60005b81524b21d6b92bae8a2d03b54e120e
Keith <kdjohn91@gmail.com>
2015-07-22 18:43:13 -0700

    Incorporated basic custom image rendering (currently not behaving as expected)

--- KSPCommEngr/FrequencyAllocation.cs
+++ KSPCommEngr/FrequencyAllocation.cs
@@ -27,19 +27,22 @@
 
 using UnityEngine;
 using System.Collections;
-using KSPCommSys.Extensions;
+using KSPCommEngr.Extensions;
 using KSP.IO;
+using System.Linq;
 
-namespace KSPCommSys
+namespace KSPCommEngr
 {
-    [KSPAddon(KSPAddon.Startup.TrackingStation, true)]
+    [KSPAddon(KSPAddon.Startup.TrackingStation, false)]
     public class FrequencyAllocation : MonoBehaviour
     {
         private static Rect windowPosition = new Rect();
         private GUIStyle windowStyle, labelStyle;
         private bool hasInitStyles = false;
+        public Texture2D image = new Texture2D(400, 100); //400 pixels wide by 100 pixels tall
 
-        public void Awake() {
+        public void Awake()
+        {
             CommEngrLog.Log("Frequency Allocation MonoBehaviour Activated");
         }
 
@@ -47,15 +50,17 @@ namespace KSPCommSys
         {
             // Signal tests with sinusoid
             Signal q = new Signal(x => Mathf.Sin(x));
-            CommEngrLog.Log("Frequency Allocation - Sine Signal generated; displayed next:");
-            q.DisplaySignal();
+            CommEngrLog.Log("Frequency Allocation - Sine Signal generated.");
+            //q.DisplaySignal();
 
             // Test channel noise distortion
             q = Channel.AdditiveNoise(q);
-            CommEngrLog.Log("Frequency Allocation - Channel Noise added to Signal ; displayed next:");
-            q.DisplaySignal();
+            CommEngrLog.Log("Frequency Allocation - Channel Noise added to Signal.");
+            //q.DisplaySignal();
 
             if (!hasInitStyles) InitStyles();
+
+            drawImage();
             RenderingManager.AddToPostDrawQueue(0, OnDraw);
         }
 
@@ -66,7 +71,8 @@ namespace KSPCommSys
 
         private void OnDraw()
         {
-            windowPosition = GUILayout.Window(10, windowPosition, OnWindow, "This is a title", windowStyle);
+
+            windowPosition = GUILayout.Window(10, windowPosition, OnWindow, "Frequency Allocation Chart", windowStyle);
 
             if (windowPosition.x == 0f && windowPosition.y == 0f)
                 windowPosition = windowPosition.CenterScreen();
@@ -74,9 +80,10 @@ namespace KSPCommSys
 
         private void OnWindow(int windowId)
         {
-            GUILayout.BeginHorizontal();
-            GUILayout.Label("Frequency Allocation Chart", labelStyle);
-            GUILayout.EndHorizontal();
+            GUILayout.BeginVertical();
+                GUI.DrawTexture(new Rect(10f, 10f, 400f, 100f), image, ScaleMode.StretchToFill);
+                GUILayout.Label("Frequency in Hz", labelStyle);
+            GUILayout.EndVertical();
 
             GUI.DragWindow();
         }
@@ -84,12 +91,44 @@ namespace KSPCommSys
         private void InitStyles()
         {
             windowStyle = new GUIStyle(HighLogic.Skin.window);
-            windowStyle.fixedWidth = 500f;
+            windowStyle.fixedWidth = 900f;
+            windowStyle.fixedHeight = 900f;
 
             labelStyle = new GUIStyle(HighLogic.Skin.label);
             labelStyle.stretchWidth = true;
+            //labelStyle.DrawCursor
 
             hasInitStyles = true;
+        }
+
+        // Code provided from KSP Wiki @: http://wiki.kerbalspaceprogram.com/wiki/Module_code_examples
+        // Special thanks to the author of the "Drawing and displaying a 2D image" snippet
+        void drawImage()
+        {
+            // Set all the pixels to black
+            int size = image.height * image.width;
+
+            Color[] colors = Enumerable.Repeat(Color.black, size).ToArray();
+            image.SetPixels(colors);
+
+            int lineWidth = 3; //draw a curve 3 pixels wide
+            for (int x = 0; x < image.width; x++)
+            {
+                int fx = f(x);
+                for (int y = fx; y < fx + lineWidth; y++)
+                {
+                    image.SetPixel(x, y, Color.red);
+                    CommEngrLog.Log("drawImage() - (" + x.ToString() + ", " + y.ToString() + ")");
+                }
+            }
+
+            image.Apply();
+        }
+
+        //the function to plot:
+        int f(int x)
+        {
+            return 2 * x;
         }
     }
 }
