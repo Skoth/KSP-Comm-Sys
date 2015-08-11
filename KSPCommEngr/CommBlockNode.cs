f87f72a4bfbdcada1daa01e6ed29cb14596189f4
Skoth <kdjohn91@gmail.com>
2015-08-10 21:19:02 -0700

    Reconfigured blocks to use Button GUIStyle, attempting a few different edge drawing schemes (none work thus far)

--- KSPCommEngr/CommBlockNode.cs
+++ KSPCommEngr/CommBlockNode.cs
@@ -28,6 +28,7 @@
 using System;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Collections.Generic;
 using UnityEngine;
 using KSP;
@@ -38,7 +39,8 @@ namespace KSPCommEngr
     public enum nodeFace
     {
         BandpassFilter,
-        DataSource, // spec for satellite vs ground?
+        DataSource,
+        DataStore,
         HighpassFilter,
         LocalOscillator,
         LowpassFilter,
@@ -56,11 +58,13 @@ namespace KSPCommEngr
             { "Bottom", new Rect(30f, 60f, 0f, 0f) },
             { "Left", new Rect(0f, 30f, 0f, 0f) }
         };
+        public CommBlockNode Connection = null;
 
         public Rect Position { get; set; }
-        public EdgeNode(Rect pos)
+        public EdgeNode(Rect pos, CommBlockNode conn = null)
         {
             Position = pos;
+            if (conn != null) Connection = conn;
         }
     }
 
@@ -78,6 +82,7 @@ namespace KSPCommEngr
             { "Left", new EdgeNode(new Rect(0f, 0f, 10f, 10f)) },
         };
         public IEnumerable<CommBlockNode> AdjacentBlocks;
+        private Vector2? offsetPos = null;
         private bool blockSelected = false;
         private bool edgeNodeHovered = false;
         private bool edgeNodeSelected = false;
@@ -87,11 +92,43 @@ namespace KSPCommEngr
         {
             Face = nf;
             Position = pos;
-            Icon = GameDatabase.Instance.GetTexture("CommEngr/Textures/Icon", false);
-
+            Icon = GameDatabase.Instance.GetTexture(String.Format("CommEngr/Textures/{0}", Face.ToString()), false);
+            
             UpdateEdgeNodes();
         }
 
+        private static GUIStyle GetBlockStyle(Texture2D tex)
+        {
+            return new GUIStyle()
+            {
+                active = HighLogic.Skin.button.active,
+                alignment = TextAnchor.MiddleCenter,
+                border = new RectOffset(0, 0, 0, 0),
+                clipping = TextClipping.Clip,
+                contentOffset = new Vector2(0, 0),
+                fixedHeight = tex.height,
+                fixedWidth = tex.width,
+                focused = HighLogic.Skin.window.focused,
+                font = null,
+                hover = HighLogic.Skin.button.hover,
+                imagePosition = ImagePosition.ImageOnly,
+                margin = new RectOffset(0, 0, 0, 0),
+                name = tex.name,
+                normal = HighLogic.Skin.button.normal,
+                onActive = new GUIStyleState() { background = HighLogic.Skin.button.active.background, textColor = Color.white },
+                onFocused = new GUIStyleState() { background = HighLogic.Skin.button.focused.background, textColor = Color.white },
+                onHover = new GUIStyleState() { background = HighLogic.Skin.button.hover.background, textColor = Color.white },
+                onNormal = new GUIStyleState() { background = HighLogic.Skin.button.normal.background, textColor = Color.white },
+                overflow = new RectOffset(0, 0, 0, 0),
+                padding = new RectOffset(0, 0, 0, 0),
+                richText = false,
+                stretchHeight = false,
+                stretchWidth = false,
+                wordWrap = true
+            };
+        }
+
+        
         public void DrawBlockNode()
         {
             // Draggable Block Node
@@ -103,24 +140,26 @@ namespace KSPCommEngr
             if (blockSelected && Event.current.type == EventType.MouseUp)
             {
                 blockSelected = false;
+                offsetPos = null;
             }
             
             if (blockSelected)
             {
                 guiDepth = 1;
-                // Do not enforce centering
-                //Position.x = Mouse.screenPos.x - Position.width / 2;
-                //Position.y = Mouse.screenPos.y - Position.height / 2;
 
-                Position.x = Mouse.screenPos.x;
-                Position.y = Mouse.screenPos.y;
+                // Enables drag and drop from any section of the block clicked
+                if (offsetPos == null)
+                    offsetPos = new Vector2(Mouse.screenPos.x - Position.x, Mouse.screenPos.y - Position.y);
+
+                Position.x = Mouse.screenPos.x - offsetPos.Value.x;
+                Position.y = Mouse.screenPos.y - offsetPos.Value.y;
                 UpdateEdgeNodes();
             }
             else guiDepth = 2;
 
             // Render Block Node, followed by its connectors
             GUI.depth = guiDepth;
-            if (GUI.Button(Position, Icon, HighLogic.Skin.window))
+            if (GUI.Button(Position, Icon, GetBlockStyle(Icon)))
             {
                 CommEngrUtils.Log(String.Format("Block Node with Rect({0}, {1}, {2}, {3}) clicked!",
                     Position.x, Position.y, Position.width, Position.height));
@@ -138,16 +177,42 @@ namespace KSPCommEngr
                 if (edgeNodeHovered && Event.current.type == EventType.MouseDown)
                     edgeNodeSelected = true;
 
-                if (edgeNodeSelected && Event.current.type == EventType.MouseDown)
+                if (edgeNodeSelected && Event.current.type == EventType.MouseUp)
                 {
+                    screenPt1 = PlanetariumCamera.Camera.ScreenToWorldPoint(new Vector3(edgeNode.Value.Position.x, edgeNode.Value.Position.y, PlanetariumCamera.Camera.transform.position.z));
+                    screenPt2 = PlanetariumCamera.Camera.ScreenToWorldPoint(new Vector3(Mouse.screenPos.x, Mouse.screenPos.y, PlanetariumCamera.Camera.transform.position.z));
+
+                    screenPt1 -= new Vector3(0f, 0f, 5f);
+                    screenPt2 -= new Vector3(0f, 0f, 5f);
+
+                    callDrawEdge = true;
+                    CommEngrUtils.Log("DrawLine initiating with red line...");
+                    
+
+                    //DrawConnection(new Vector2(edgeNode.Value.Position.x, edgeNode.Value.Position.y), Mouse.screenPos, Color.red, 3.0f);
+                    //DrawLine(new Vector2(edgeNode.Value.Position.x, edgeNode.Value.Position.y), Mouse.screenPos, Color.red, 3f);
+                    //CommEngrUtils.Log("GL Lines initiating with blue line...");
+                    
+                    //GL.PushMatrix();
+                    //GL.Begin(GL.LINES);
+                    //    GL.Color(Color.blue);
+                    //    GL.Vertex(new Vector3(edgeNode.Value.Position.x, edgeNode.Value.Position.y, 0f));
+                    //    GL.Vertex(new Vector3(Mouse.screenPos.x, Mouse.screenPos.y, 0f));
+                    //GL.End();
+                    //GL.PopMatrix();
+                    //CommEngrUtils.Log("GUI Label initiating with green line...");
+                    //Texture2D tempTex = new Texture2D(5, 5);
+                    //tempTex.SetPixels(Enumerable.Repeat(Color.green, tempTex.width * tempTex.height).ToArray());
+                    //GUIUtility.ScaleAroundPivot(new Vector2(), new Vector2());
+                    //GUI.Label(new Rect(200, 200, 10, 10), tempTex);
                     edgeNodeHovered = false;
                     edgeNodeSelected = false;
                 }
 
-                // Render Edge Nodes
+                // Render Edge Node Buttons
                 // Test opacity rendering
                 Color temp = GUI.color;
-                GUI.color = new Color(1f, 0f, 0f, 0.5f);
+                GUI.color = new Color(temp.r, temp.g, temp.b, 0.5f);
                 if (GUI.Button(edgeNode.Value.Position, "EN"))
                 {
                     CommEngrUtils.Log(String.Format("Edge Node with Rect({0}, {1}, {2}, {3}) clicked!",
@@ -155,6 +220,22 @@ namespace KSPCommEngr
                         edgeNode.Value.Position.width, edgeNode.Value.Position.height));
                 }
                 GUI.color = temp;
+
+                if (callDrawEdge) CommBlockNode.DrawEdge();
+            }
+        }
+        private static Vector3 screenPt1;
+        private static Vector3 screenPt2;
+        private static void DrawEdge()
+        {
+            if (screenPt1 != null && screenPt2 != null) {
+                GL.PushMatrix();
+                GL.Begin(GL.LINES);
+                    GL.Color(Color.red);
+                    GL.Vertex3(screenPt1.x, screenPt1.y, screenPt1.z);
+                    GL.Vertex3(screenPt2.x, screenPt2.y, screenPt2.z);
+                GL.End();
+                GL.PopMatrix();
             }
         }
 
@@ -172,5 +253,92 @@ namespace KSPCommEngr
         }
 
 
+        private static Rect lineRect = new Rect(0, 0, 1, 1);
+        private static Texture2D lineTex = null;
+        private static Material blitMat = null;
+        private static Material blendMat = null;
+        private bool callDrawEdge = false;
+
+        private static void initConnection()
+        {
+            lineTex = new Texture2D(1, 1, TextureFormat.ARGB32, false);
+            lineTex.SetPixel(0, 1, Color.red);
+            lineTex.Apply();
+            blitMat = (Material)typeof(GUI).GetMethod("get_blitMaterial", BindingFlags.NonPublic | BindingFlags.Static).Invoke(null, null);
+            blendMat = (Material)typeof(GUI).GetMethod("get_blendMaterial", BindingFlags.NonPublic | BindingFlags.Static).Invoke(null, null);
+            return;
+        }
+        private static void DrawConnection(Vector2 pointA, Vector2 pointB, Color color, float width)
+        {
+            if (lineTex == null) initConnection();
+            float dx = pointB.x - pointA.x;
+            float dy = pointB.y - pointA.y;
+            float len = Mathf.Sqrt(dx * dx + dy * dy);
+            if (len < 0.001f) return;
+            Texture2D tex = lineTex;
+            Material mat = blitMat;
+            float wdx = width * dy / len;
+            float wdy = width * dx / len;
+            /*
+                [ 00 , 01, 02, 03 ]
+                [ 10 , 11, 12, 13 ]
+                [ 20 , 21, 22, 23 ]
+                [ 30 , 31, 32, 33 ]
+            */
+            Matrix4x4 matrix = Matrix4x4.identity;
+            matrix.m00 = dx;
+            matrix.m01 = -wdx;
+            matrix.m03 = pointA.x + 0.5f * wdx;
+            matrix.m10 = dy;
+            matrix.m11 = wdy;
+            matrix.m13 = pointA.y - 0.5f * wdy;
+            GL.PushMatrix();
+            GL.MultMatrix(matrix);
+            GUI.color = color;
+            GUI.DrawTexture(lineRect, tex);
+            GL.PopMatrix();
+        }
+
+        //public static void DrawLine(Vector2 pointA, Vector2 pointB, Color color, float width)
+        //{
+        //    // Save the current GUI matrix, since we're going to make changes to it.
+        //    Matrix4x4 matrix = GUI.matrix;
+
+        //    // Generate a single pixel texture if it doesn't exist
+        //    if (!lineTex) { lineTex = new Texture2D(1, 1); }
+
+        //    // Store current GUI color, so we can switch it back later,
+        //    // and set the GUI color to the color parameter
+        //    Color savedColor = GUI.color;
+        //    GUI.color = color;
+
+        //    // Determine the angle of the line.
+        //    float angle = Vector3.Angle(pointB - pointA, Vector2.right);
+
+        //    // Vector3.Angle always returns a positive number.
+        //    // If pointB is above pointA, then angle needs to be negative.
+        //    if (pointA.y > pointB.y) { angle = -angle; }
+
+        //    // Use ScaleAroundPivot to adjust the size of the line.
+        //    // We could do this when we draw the texture, but by scaling it here we can use
+        //    //  non-integer values for the width and length (such as sub 1 pixel widths).
+        //    // Note that the pivot point is at +.5 from pointA.y, this is so that the width of the line
+        //    //  is centered on the origin at pointA.
+        //    GUIUtility.ScaleAroundPivot(new Vector2((pointB - pointA).magnitude, width), new Vector2(pointA.x, pointA.y + 0.5f));
+
+        //    // Set the rotation for the line.
+        //    //  The angle was calculated with pointA as the origin.
+        //    GUIUtility.RotateAroundPivot(angle, pointA);
+
+        //    // Finally, draw the actual line.
+        //    // We're really only drawing a 1x1 texture from pointA.
+        //    // The matrix operations done with ScaleAroundPivot and RotateAroundPivot will make this
+        //    //  render with the proper width, length, and angle.
+        //    GUI.DrawTexture(new Rect(pointA.x, pointA.y, 1, 1), lineTex);
+
+        //    // We're done.  Restore the GUI matrix and GUI color to whatever they were before.
+        //    GUI.matrix = matrix;
+        //    GUI.color = savedColor;
+        //}
     }
 }
