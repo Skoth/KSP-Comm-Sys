fcf86c2bb0eb03b4bffe18f6e023ccc9f3adecb4
Skoth <kdjohn91@gmail.com>
2015-08-21 06:40:37 -0700

    Finally got working DrawConnection() method in place

--- KSPCommEngr/CommBlockNode.cs
+++ KSPCommEngr/CommBlockNode.cs
@@ -68,6 +68,10 @@ namespace KSPCommEngr
         private bool edgeNodeHovered = false;
         private bool edgeNodeSelected = false;
         private int guiDepth = 2;
+        private static Vector2 screenPt1;
+        private static Vector2 screenPt2;
+        private static Rect initPos;
+        private bool callDrawEdge;
 
         public CommBlockNode(nodeFace nf, Rect pos)
         {
@@ -155,44 +159,48 @@ namespace KSPCommEngr
                 else
                     edgeNodeHovered = false;
 
-                if (edgeNodeHovered && Event.current.type == EventType.MouseDown)
+                if (edgeNodeHovered && Input.GetMouseButtonDown(0)) {
+                    screenPt1 = Input.mousePosition;
                     edgeNodeSelected = true;
-
-                if (edgeNodeSelected && Event.current.type == EventType.MouseUp)
-                {
-                    screenPt1 = new Vector2(edgeNode.Value.Position.x, edgeNode.Value.Position.y);
-                    screenPt2 = new Vector2(Mouse.screenPos.x, Mouse.screenPos.y);
-
                     callDrawEdge = true;
-                    CommEngrUtils.Log("DrawLine initiating with red line...");
+                }
 
+                if (edgeNodeSelected && Input.GetMouseButtonDown(1))
+                {
+                    callDrawEdge = false;
                     edgeNodeHovered = false;
                     edgeNodeSelected = false;
                 }
 
                 // Render Edge Node Buttons
                 // Test opacity rendering
-                Color temp = GUI.color;
-                GUI.color = new Color(temp.r, temp.g, temp.b, 0.5f);
+                //Color temp = GUI.color;
+                //GUI.color = new Color(temp.r, temp.g, temp.b, 0.5f);
                 if (GUI.Button(edgeNode.Value.Position, "EN"))
                 {
-                    CommEngrUtils.Log(String.Format("Edge Node with Rect({0}, {1}, {2}, {3}) clicked!",
-                        edgeNode.Value.Position.x, edgeNode.Value.Position.y,
-                        edgeNode.Value.Position.width, edgeNode.Value.Position.height));
+                    //CommEngrUtils.Log(String.Format("Edge Node with Rect({0}, {1}, {2}, {3}) clicked!",
+                    //    edgeNode.Value.Position.x, edgeNode.Value.Position.y,
+                    //    edgeNode.Value.Position.width, edgeNode.Value.Position.height));
                 }
-                GUI.color = temp;
+
+                if (edgeNodeSelected)
+                {
+                    //Texture2D tempTex = new Texture2D(1, 1, TextureFormat.ARGB32, false);
+                    //tempTex.SetPixel(0, 0, Color.cyan);
+                }
 
                 if (callDrawEdge)
                 {
-                    Texture2D tempTex = new Texture2D(5, 5, TextureFormat.ARGB32, false);
-                    tempTex.SetPixels(0, 0, 2, 2, Enumerable.Repeat(Color.cyan, 9).ToArray());
-                    GUI.DrawTexture(new Rect(300f, 300f, 100f, 100f), tempTex, ScaleMode.ScaleToFit, true);
-                    CommBlockNode.DrawConnection(screenPt1, screenPt2, Color.red, 5f);
+                    // Dragged Line
+                    //Texture2D tempTex = new Texture2D(5, 5, TextureFormat.ARGB32, false);
+                    //tempTex.SetPixels(0, 0, 2, 2, Enumerable.Repeat(Color.cyan, 9).ToArray());
+                    //GUI.DrawTexture(new Rect(Mouse.screenPos.x, Mouse.screenPos.y, 50f, 50f), tempTex, ScaleMode.ScaleToFit, true);
+
+                    CommBlockNode.DrawConnection(screenPt1, Mouse.screenPos, Color.red, 2f);
+                    //callDrawEdge = false;
                 }
             }
         }
-        private static Vector2 screenPt1;
-        private static Vector2 screenPt2;
 
         private void UpdateEdgeNodes()
         {
@@ -207,50 +215,19 @@ namespace KSPCommEngr
             }
         }
 
-        private static Texture2D lineTex = null;
-        private bool callDrawEdge = false;
-
-        // First and only working GUI Line rendering (replacement with GL.LINES?)
-        public static void DrawConnection(Vector2 pointA, Vector2 pointB, Color color, float width)
+        public static void DrawConnection(Vector2 ptA, Vector2 ptB, Color color, float width)
         {
-            // Save the current GUI matrix, since we're going to make changes to it.
-            Matrix4x4 matrix = GUI.matrix;
-
-            // Generate a single pixel texture if it doesn't exist
-            if (!lineTex) { lineTex = new Texture2D(1, 1); }
-
-            // Store current GUI color, so we can switch it back later,
-            // and set the GUI color to the color parameter
             Color savedColor = GUI.color;
             GUI.color = color;
-
-            // Determine the angle of the line.
-            float angle = Vector3.Angle(pointB - pointA, Vector2.right);
-
-            // Vector3.Angle always returns a positive number.
-            // If pointB is above pointA, then angle needs to be negative.
-            if (pointA.y > pointB.y) { angle = -angle; }
-
-            // Use ScaleAroundPivot to adjust the size of the line.
-            // We could do this when we draw the texture, but by scaling it here we can use
-            //  non-integer values for the width and length (such as sub 1 pixel widths).
-            // Note that the pivot point is at +.5 from pointA.y, this is so that the width of the line
-            //  is centered on the origin at pointA.
-            GUIUtility.ScaleAroundPivot(new Vector2((pointB - pointA).magnitude, width), new Vector2(pointA.x, pointA.y + 0.5f));
-
-            // Set the rotation for the line.
-            //  The angle was calculated with pointA as the origin.
-            GUIUtility.RotateAroundPivot(angle, pointA);
-
-            // Finally, draw the actual line.
-            // We're really only drawing a 1x1 texture from pointA.
-            // The matrix operations done with ScaleAroundPivot and RotateAroundPivot will make this
-            //  render with the proper width, length, and angle.
-            GUI.DrawTexture(new Rect(pointA.x, pointA.y, 1, 1), lineTex);
-
-            // We're done.  Restore the GUI matrix and GUI color to whatever they were before.
-            GUI.matrix = matrix;
+            Matrix4x4 savedMatrix = GUI.matrix;
+            Vector2 rectPtA = new Vector2(ptA.x, Screen.height - ptA.y);
+            float angle = (ptB.y < rectPtA.y) ? -Vector2.Angle(ptB - rectPtA, Vector2.right) : Vector2.Angle(ptB - rectPtA, Vector2.right);
+            GUIUtility.RotateAroundPivot(angle, rectPtA);
+            float mag = (ptB - rectPtA).magnitude;
+            Texture2D lineTex = new Texture2D(1, 1);
+            GUI.DrawTexture(new Rect(ptA.x, rectPtA.y, mag, width), lineTex);
             GUI.color = savedColor;
+            GUI.matrix = savedMatrix;
         }
     }
 }
