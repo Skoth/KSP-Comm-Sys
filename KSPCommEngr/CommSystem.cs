a1239ff4b6ff1c700fe17150d423442432176f28
Skoth <kdjohn91@gmail.com>
2015-08-21 06:41:39 -0700

    Refactored and modified such that the class no longer derives from MonoBehaviour (unnecessary for its purposes)

--- KSPCommEngr/CommSystem.cs
+++ KSPCommEngr/CommSystem.cs
@@ -2,38 +2,36 @@
 using System.Linq;
 using KSP;
 using UnityEngine;
+using System.Xml.Linq;
 
 namespace KSPCommEngr
 {
-    [KSPAddon(KSPAddon.Startup.TrackingStation, false)]
-    public class CommSystem : MonoBehaviour
+    public class CommSystem
     {
         public List<CommBlockNode> blockDiagram = new List<CommBlockNode>();
 
-        public void Awake()
+        public CommSystem(XDocument xmlCSDoc = null)
         {
-            CommEngrUtils.Log("CommSystem Awake()");
+            if (xmlCSDoc == null)
+            {
+                // Simple DSB Receiver
+                CommEngrUtils.Log("CommSystem: Allocation for DSB Receiver Start");
+                blockDiagram.AddRange(new CommBlockNode[] {
+                    new CommBlockNode(nodeFace.DataSource, new Rect(400f, 400f, 60f, 60f)),
+                    new CommBlockNode(nodeFace.Multiplier, new Rect(500f, 500f, 60f, 60f)),
+                    new CommBlockNode(nodeFace.LocalOscillator, new Rect(600f, 600f, 60f, 60f)),
+                    new CommBlockNode(nodeFace.LowpassFilter, new Rect(700f, 700f, 60f, 60f)),
+                    new CommBlockNode(nodeFace.DataStore, new Rect(800f, 800f, 60f, 60f)) { }
+                });
+                CommEngrUtils.Log("CommSystem: Allocation for DSB Receiver End");
+            }
         }
 
-        public void Start()
-        {
-            // Simple DSB Receiver
-            CommEngrUtils.Log("CommSystem: Allocation for DSB Receiver Start");
-            blockDiagram.AddRange(new CommBlockNode[] {
-                new CommBlockNode(nodeFace.DataSource, new Rect(400f, 400f, 60f, 60f)),
-                new CommBlockNode(nodeFace.Multiplier, new Rect(500f, 500f, 60f, 60f)),
-                new CommBlockNode(nodeFace.LocalOscillator, new Rect(600f, 600f, 60f, 60f)),
-                new CommBlockNode(nodeFace.LowpassFilter, new Rect(700f, 700f, 60f, 60f)),
-                new CommBlockNode(nodeFace.DataStore, new Rect(800f, 800f, 60f, 60f)) { }
-            });
-            CommEngrUtils.Log("CommSystem: Allocation for DSB Receiver End");
-        }
-        
-        public void OnGUI()
+        public void DrawCommSystem()
         {
             GUI.Box(new Rect(210f, 850f, ((float)Screen.width) - 310f, ((float)Screen.height) - 810f), "System Diagram");
 
-            foreach(var block in blockDiagram)
+            foreach (var block in blockDiagram)
             {
                 block.DrawBlockNode();
             }
@@ -55,7 +53,7 @@ namespace KSPCommEngr
         {
             foreach (var node in blockDiagram)
             {
-                if(node.Equals(new Object()))
+                if (node.Equals(new Object()))
                     return node;
             }
             return null;
