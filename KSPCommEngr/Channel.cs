ad2537ac5bc9b14f5dfb9c0a0e7ac3b042dfedfd
Skoth <kdjohn91@gmail.com>
2015-09-11 11:35:20 -0700

    Channel class moved to own file, currently written as extension method container for Signal

---
+++ KSPCommEngr/Channel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KSPCommEngr
+{
+    public static class Channel
+    {
+        public static void AWGN(this Signal x)
+        {
+            for (int i = 0; i < x.Size; ++i)
+            {
+                x[i] = x[i] + UnityEngine.Random.value;
+            }
+        }
+    }
+}
