f2b624018b36c286a1e992f3cbfc70fe8f1170c4
Skoth <kdjohn91@gmail.com>
2016-01-21 21:03:35 -0800

    Test for async graph updates from gui block/connector

---
+++ CommEngrTest/CommBlockTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KSPCommEngr;
+
+namespace CommEngrTest
+{
+    [TestClass]
+    public class CommBlockTests
+    {
+        [TestMethod]
+        public void UpdateGraphOnDragStop()
+        {
+            Graph graph = new Graph(new int[,] {
+                { 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0 }
+            });
+            Graph expectedGraph = new Graph(new int[,] {
+                { 0, 0, 0, 0, 0 },
+                { 0, 1, 1, 1, 0 },
+                { 0, 1, 1, 1, 0 },
+                { 0, 1, 1, 1, 0 },
+                { 0, 0, 0, 0, 0 }
+            });
+
+            // User Drag and Drop Event
+
+            CollectionAssert.AreEqual(expectedGraph.nodes, graph.nodes);
+        }
+    }
+}
